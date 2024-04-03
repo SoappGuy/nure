076360661d5eb6e2e70966e13186fcd0f2713e71
dumbnerd <107756166+SoappGuy@users.noreply.github.com>
2024-04-03 17:50:43 +0300

    added gitignore

---
+++ DSaA/rust/bbst/src/main.rs
@@ -0,0 +1,21 @@
+#![allow(unused)]
+
+#[macro_use]
+mod bbst;
+
+use crate::bbst::*;
+use anyhow::Result;
+use rand::random;
+
+fn main() -> Result<()> {
+    // let mut bbst = bbst![0, 2, 3, 1, -2, -3, -1];
+    // let mut bbst = bbst![10, 5, 11, 4, 7, 3, 2, 6];
+    let mut bbst = rand_bbst![10];
+
+    // let mut bbst = Bbst::new();
+
+    println!("{bbst}");
+    println!("{}", bbst.count_left());
+
+    Ok(())
+}
