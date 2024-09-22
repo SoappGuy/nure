eaf01602eaa744b475d14e5732b2a67eb95eff41
Yehor Sytnyk <github@dev.cuniling.us>
2024-09-22 13:11:04 +0300

    551251/A - accepted

--- DSaA/2024-2025/contests/551251/A. Машина времени/main.go
+++ DSaA/2024-2025/contests/551251/A. Машина времени/main.go
@@ -2,42 +2,37 @@ package main
 
 import (
 	"fmt"
-	"sort"
 )
 
 func main() {
 	var N int
 	fmt.Scanln(&N)
 
-	values := map[int][]int{1: {1}}
-	found := find(N, &values)
+	steps := map[int]int{1: 0}
+	prevs := map[int]int{1: 0}
 
-	fmt.Println(len(found) - 1)
-	for _, val := range found {
-		fmt.Println(val)
-	}
-}
+	for i := 2; i <= N; i++ {
+		steps[i] = steps[i-1] + 1
+		prevs[i] = i - 1
 
-func find(n int, values *map[int][]int) []int {
-	_, exist := (*values)[n]
-
-	if !exist {
-		m := [][]int{}
-
-		if n%3 == 0 {
-			m = append(m, find(n/3, values))
+		if i%2 == 0 && steps[i/2]+1 < steps[i] {
+			steps[i] = steps[i/2] + 1
+			prevs[i] = i / 2
 		}
-		if n%2 == 0 {
-			m = append(m, find(n/2, values))
+
+		if i%3 == 0 && steps[i/3]+1 < steps[i] {
+			steps[i] = steps[i/3] + 1
+			prevs[i] = i / 3
 		}
-		m = append(m, find(n-1, values))
-
-		sort.Slice(m, func(i, j int) bool {
-			return len(m[i]) < len(m[j])
-		})
-
-		(*values)[n] = append(m[0], n)
 	}
 
-	return (*values)[n]
+	answer := []int{}
+	for curr := N; curr != 0; curr = prevs[curr] {
+		answer = append([]int{curr}, answer...)
+	}
+
+	fmt.Println(len(answer) - 1)
+	for i := 0; i < len(answer); i++ {
+		fmt.Println(answer[i])
+	}
 }
